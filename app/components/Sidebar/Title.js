a207e8cea1f94bb64753ed51d0f8b030c48fb012
Ankit Muchhala <muchhalaankit@gmail.com>
2016-08-07 00:45:14 +0530

    fix title component

--- app/components/Sidebar/Title.js
+++ app/components/Sidebar/Title.js
@@ -6,8 +6,8 @@ const Title = () => (
       <img className="media-object img-circle" width="40px" height="40px" src="http://lorempixel.com/200/200/" alt="..." />
     </div>
     <div className="media-body">
-      <h5 className="media-heading"><b>Ankit Muchhala</b></h5>
-      <span className="text-muted">online</span>
+      <h5 className="media-heading" style={{ margin: '5px 0 0 0' }}><b>Ankit Muchhala</b></h5>
+      <small className="text-muted">online</small>
     </div>
   </div>
 );
