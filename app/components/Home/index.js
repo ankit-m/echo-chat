54b8a8873eb60632d808ae335480155bbd14ab4f
Ankit Muchhala <muchhalaankit@gmail.com>
2016-08-09 04:40:20 +0530

    change home message

--- app/components/Home/index.js
+++ app/components/Home/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => (
   <div className="text-center text-muted" style={{ marginTop: '120px' }}>
     <span className="glyphicon glyphicon-hand-left" />
-    &nbsp;Select a friend to chat
+    &nbsp;Add/select a friend to chat
   </div>
 );
 
