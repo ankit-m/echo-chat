9bacc0eb62cd8b13c807bf0fea5233e8cd1f777c
Ankit Muchhala <muchhalaankit@gmail.com>
2016-08-09 04:25:42 +0530

    final test cases

--- test/components/Chatbox/index.js
+++ test/components/Chatbox/index.js
@@ -1,1 +1,33 @@
+import React from 'react';
+import { expect } from 'chai';
+import { mount } from 'enzyme';
+import { WebSocket } from 'mock-socket';
+import Chatbox from '../../../app/components/Chatbox';
 
+describe('Chatbox', () => {
+  localStorage.setItem('friendList', JSON.stringify({ uid1: { name: 'test', image: '' } }));
+  localStorage.setItem('uid1', JSON.stringify({ name: 'test', messages: [] }));
+  global.WebSocket = WebSocket;
+  const wrapper = mount(<Chatbox routeParams={{ userId: 'uid1' }} />);
+  it('should render ChatName and Messages all the time', () => {
+    expect(wrapper.find('ChatName').length).to.equal(1);
+    expect(wrapper.find('Messages').length).to.equal(1);
+  });
+  it('should render Loader if state is loading', () => {
+    wrapper.setState({ load: true, error: false });
+    expect(wrapper.find('Loader').length).to.equal(1);
+  });
+  it('should render Errorbar if state is error', () => {
+    wrapper.setState({ load: false, error: true });
+    expect(wrapper.find('Errorbar').length).to.equal(1);
+  });
+  it('should render input bar in normal state', () => {
+    wrapper.setState({ load: false, error: false });
+    expect(wrapper.find('MessageInput').length).to.equal(1);
+  });
+  it('should add new messages to localStorage', () => {
+    wrapper.instance().addNewMessage('test', 1);
+    expect(JSON.parse(localStorage.uid1).messages[0].message).to.equal('test');
+    expect(JSON.parse(localStorage.uid1).messages[0].type).to.equal(1);
+  });
+});
--- test/components/Main.js
+++ test/components/Main.js
@@ -4,14 +4,19 @@ import { mount } from 'enzyme';
 import Main from '../../app/components/Main';
 
 describe('Main', () => {
-  const wrapper = mount(<Main children={[]} />);
+  beforeEach(() => {
+    localStorage.clear();
+  });
+  it('should set localStorage if it is empty', () => {
+    mount(<Main children={[]} />);
+    expect(localStorage).to.deep.equal({ friendList: '{}' });
+  });
   it('should render Sidebar', () => {
+    const wrapper = mount(<Main children={[]} />);
     expect(wrapper.find('Sidebar').length).to.equal(1);
   });
   it('should render content-wrapper', () => {
+    const wrapper = mount(<Main children={[]} />);
     expect(wrapper.find('#content-wrapper').length).to.equal(1);
-  });
-  it('should set localStorage if it is empty', () => {
-    expect(localStorage).to.deep.equal({ friendList: '{}' });
   });
 });
